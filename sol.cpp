3c5e06d5b1ea2d5ab4ccb0a5fb858844e6c15053
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-01-31 22:00:49 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+                             
+int main() {                            
+  ios::sync_with_stdio(false);
+  cin.tie(0);          
+  int n, t;
+  cin >> n >> t;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+  	cin >> a[i];
+  }                         
+  int cnt = 0;
+  for (int i = 0; i < n; i++) {
+  	// while the time required to read is > 0
+  	// available time for each day = 86400 - a[i] (for all 'i')
+  	// keep subtracting the available time for each day
+  	// increment the counter for each day passed
+  	if (t > 0) {
+  		t -= (86400 - a[i]);
+  		cnt++;
+  	}
+  }
+  cout << cnt << '\n';
+  return 0;
+}      
